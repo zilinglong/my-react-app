43e3a6f0268395f3df5a3c8ab16b89c6f1b48c92
孟明秀 <mengmingxiu@100tal.com>
2020-12-23 11:23:09 +0800

    feat: dialog中嵌套form表单，点击取消关闭弹窗，点击确定获取表单内容

--- src/views/Login/components/Dialog/index.js
+++ src/views/Login/components/Dialog/index.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
-import { Modal, Button, Form, Select, Input } from 'antd';
+// import { Modal, Button, Form, Select, Input } from 'antd';
+import { Modal, Form, Select, InputNumber, Switch, Radio,  Slider, Button, Upload, Rate, Checkbox, Row, Col } from 'antd';
+import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
 const { Option } = Select;
+
+const normFile = (e) => {
+  console.log('Upload event:', e);
+
+  if (Array.isArray(e)) {
+    return e;
+  }
+
+  return e && e.fileList;
+};
 class DialogCreateEdit extends Component {
   constructor(props) {
     super(props);
@@ -31,11 +43,17 @@ class DialogCreateEdit extends Component {
   showModal = () => {
     this.setVisible(true);
   };
-  handleOk = () => {
+  form = React.createRef();
+  handleOk = async () => {
     console.log('handleOk');
+    const fieldsValue = await this.form.current.validateFields(); 
+    //fieldsValue即为表单内的值
+    console.log("okHandle -> fieldsValue", fieldsValue)
     this.setVisible(false);
   };
-
+  onReset = () => {
+    this.form.current.resetFields();
+  };
   handleCancel = () => {
     console.log('handleCancel');
     this.setVisible(false);
@@ -51,14 +69,21 @@ class DialogCreateEdit extends Component {
 
   render() {
     const { visible, type } = this.state;
-    // 设置label和表单区域占据的宽度
-    const labelCol = 6;
-    const layout = {
-      labelCol: { span: labelCol },
-      wrapperCol: { span: 16 },
+    // // 设置label和表单区域占据的宽度
+    const labelCol = 8;
+    const formItemLayout = {
+      labelCol: {
+        span: labelCol
+      },
+      wrapperCol: {
+        span: 14,
+      },
     };
     const tailLayout = {
       wrapperCol: { offset: labelCol, span: 16 },
+    };
+    const checkboxStyle = {
+      lineHeight: '32px',
     };
     return (
       <div>
@@ -70,39 +95,202 @@ class DialogCreateEdit extends Component {
           visible={visible}
           onOk={this.handleOk}
           onCancel={this.handleCancel}
+          cancelText="取消"
+          okText="确定"
         >
           <section className="form-container">
             <Form
-              {...layout}
-              name="basic"
-              className="form"
-              initialValues={{ remember: true }}
+              ref={this.form}
+              name="validate_other"
+              {...formItemLayout}
               onFinish={this.onFinish}
-              onFinishFailed={this.onFinishFailed}
+              initialValues={{
+                ['input-number']: 3,
+                ['checkbox-group']: ['A', 'B'],
+                rate: 3.5,
+                slider: 12,
+                'select-country': 'china',
+                'select-multiple': ['red'],
+                'radio-button': 'b'
+              }}
             >
-              <Form.Item
-                label="Username"
-                name="username"
-                rules={[{ required: true, message: 'Please input your username!' }]}
-              >
-                <Input />
+              <Form.Item label="Plain Text">
+                <span className="ant-form-text">China</span>
               </Form.Item>
-
               <Form.Item
-                name="select"
+                name="select-country"
                 label="Select"
                 hasFeedback
-                rules={[{ required: true, message: 'Please select your country!' }]}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please select your country!',
+                  },
+                ]}
               >
                 <Select placeholder="Please select a country">
                   <Option value="china">China</Option>
                   <Option value="usa">U.S.A</Option>
                 </Select>
               </Form.Item>
+
+              <Form.Item
+                name="select-multiple"
+                label="Select[multiple]"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please select your favourite colors!',
+                    type: 'array',
+                  },
+                ]}
+              >
+                <Select mode="multiple" placeholder="Please select favourite colors">
+                  <Option value="red">Red</Option>
+                  <Option value="green">Green</Option>
+                  <Option value="blue">Blue</Option>
+                </Select>
+              </Form.Item>
+
+              <Form.Item label="InputNumber">
+                <Form.Item name="input-number" noStyle>
+                  <InputNumber min={1} max={10} />
+                </Form.Item>
+                <span className="ant-form-text"> machines</span>
+              </Form.Item>
+
+              <Form.Item name="switch" label="Switch" valuePropName="checked">
+                <Switch />
+              </Form.Item>
+
+              <Form.Item name="slider" label="Slider">
+                <Slider
+                  marks={{
+                    0: 'A',
+                    20: 'B',
+                    40: 'C',
+                    60: 'D',
+                    80: 'E',
+                    100: 'F',
+                  }}
+                />
+              </Form.Item>
+
+              <Form.Item name="radio-group" label="Radio.Group">
+                <Radio.Group>
+                  <Radio value="1">item 1</Radio>
+                  <Radio value="2">item 2</Radio>
+                  <Radio value="3">item 3</Radio>
+                </Radio.Group>
+              </Form.Item>
+
+              <Form.Item
+                name="radio-button"
+                label="Radio.Button"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please pick an item!',
+                  },
+                ]}
+              >
+                <Radio.Group>
+                  <Radio.Button value="a">item 1</Radio.Button>
+                  <Radio.Button value="b">item 2</Radio.Button>
+                  <Radio.Button value="c">item 3</Radio.Button>
+                </Radio.Group>
+              </Form.Item>
+
+              <Form.Item name="checkbox-group" label="Checkbox.Group">
+                <Checkbox.Group>
+                  <Row>
+                    <Col span={8}>
+                      <Checkbox
+                        value="A"
+                        style={checkboxStyle}
+                      >
+                        A
+                      </Checkbox>
+                    </Col>
+                    <Col span={8}>
+                      <Checkbox
+                        value="B"
+                        style={checkboxStyle}
+                        disabled
+                      >
+                        B
+                      </Checkbox>
+                    </Col>
+                    <Col span={8}>
+                      <Checkbox
+                        value="C"
+                        style={checkboxStyle}
+                      >
+                        C
+                      </Checkbox>
+                    </Col>
+                    <Col span={8}>
+                      <Checkbox
+                        value="D"
+                        style={checkboxStyle}
+                      >
+                        D
+                      </Checkbox>
+                    </Col>
+                    <Col span={8}>
+                      <Checkbox
+                        value="E"
+                        style={checkboxStyle}
+                      >
+                        E
+                      </Checkbox>
+                    </Col>
+                    <Col span={8}>
+                      <Checkbox
+                        value="F"
+                        style={checkboxStyle}
+                      >
+                        F
+                      </Checkbox>
+                    </Col>
+                  </Row>
+                </Checkbox.Group>
+              </Form.Item>
+
+              <Form.Item name="rate" label="Rate">
+                <Rate />
+              </Form.Item>
+
+              <Form.Item
+                name="upload"
+                label="Upload"
+                valuePropName="fileList"
+                getValueFromEvent={normFile}
+                extra="longgggggggggggggggggggggggggggggggggg"
+              >
+                <Upload name="logo" action="/upload.do" listType="picture">
+                  <Button icon={<UploadOutlined />}>Click to upload</Button>
+                </Upload>
+              </Form.Item>
+
+              <Form.Item label="Dragger">
+                <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+                  <Upload.Dragger name="files" action="/upload.do">
+                    <p className="ant-upload-drag-icon">
+                      <InboxOutlined />
+                    </p>
+                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                    <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+                  </Upload.Dragger>
+                </Form.Item>
+              </Form.Item>
+
               <Form.Item
                 {...tailLayout}
               >
-                <Button type="primary" htmlType="submit">submit</Button>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
               </Form.Item>
             </Form>
           </section>
